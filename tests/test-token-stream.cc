ea82dad2f799a0e5f111d46f29c5ffa0703443be
Andreas Fett <a.fett@gmx.de>
2015-06-21 15:37:40 +0200

    TokenStream: add unit tests

--- tests/test-token-stream.cc
+++ tests/test-token-stream.cc
@@ -1,6 +1,7 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include "token-stream.h"
+#include "utf8stream.h"
 
 namespace unittests {
 namespace token_stream {
@@ -12,8 +13,16 @@ public:
 	void tearDown();
 
 private:
+	void test_structural();
+	void test_literal();
+	void test_empty_array();
+	void test_nospace_array();
 
 	CPPUNIT_TEST_SUITE(test);
+	CPPUNIT_TEST(test_structural);
+	CPPUNIT_TEST(test_literal);
+	CPPUNIT_TEST(test_empty_array);
+	CPPUNIT_TEST(test_nospace_array);
 	CPPUNIT_TEST_SUITE_END();
 };
 
@@ -27,5 +36,87 @@ void test::setUp()
 
 void test::tearDown()
 { }
+
+using namespace jsonp;
+
+void test::test_structural()
+{
+	char data[] = " { } [ ] : , ";
+	Utf8Stream us(data, sizeof(data) - 1);
+	TokenStream ts(us);
+
+	CPPUNIT_ASSERT_EQUAL(Token::INVALID, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::BEGIN_OBJECT, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::END_OBJECT, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::BEGIN_ARRAY, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::END_ARRAY, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::NAME_SEPARATOR, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::VALUE_SEPARATOR, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Utf8Stream::SEOF, us.state());
+}
+
+void test::test_literal()
+{
+	char data[] = "true false null";
+	Utf8Stream us(data, sizeof(data) - 1);
+	TokenStream ts(us);
+
+	CPPUNIT_ASSERT_EQUAL(Token::INVALID, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::TRUE_LITERAL, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::FALSE_LITERAL, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::NULL_LITERAL, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Utf8Stream::SEOF, us.state());
+}
+
+void test::test_empty_array()
+{
+	char data[] = "[]";
+	Utf8Stream us(data, sizeof(data) - 1);
+	TokenStream ts(us);
+
+	CPPUNIT_ASSERT_EQUAL(Token::INVALID, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::BEGIN_ARRAY, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::END_ARRAY, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Utf8Stream::SEOF, us.state());
+}
+
+void test::test_nospace_array()
+{
+	char data[] = "[true,false,null]";
+	Utf8Stream us(data, sizeof(data) - 1);
+	TokenStream ts(us);
+
+	CPPUNIT_ASSERT_EQUAL(Token::INVALID, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::BEGIN_ARRAY, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::TRUE_LITERAL, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::VALUE_SEPARATOR, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::FALSE_LITERAL, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::VALUE_SEPARATOR, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::NULL_LITERAL, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Token::END_ARRAY, ts.token.type);
+	ts.scan();
+	CPPUNIT_ASSERT_EQUAL(Utf8Stream::SEOF, us.state());
+}
 
 }}
