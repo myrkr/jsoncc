53cad8d2ece75b47d1e55f98eaa0670c2f78363e
Andreas Fett <a.fett@gmx.de>
2017-07-09 13:11:59 +0200

    tests: allow to run only part of the tests

--- tests/main.cc
+++ tests/main.cc
@@ -3,13 +3,49 @@
 #include <cppunit/TextTestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/CompilerOutputter.h>
+#include <unistd.h>
 
-int main()
+namespace {
+
+void printTestTree(CppUnit::Test const* root)
 {
-	CppUnit::TextTestRunner runner;
+	std::cerr << root->getName() << "\n";
+	for (int i(0); i < root->getChildTestCount(); ++i) {
+		printTestTree(root->getChildTestAt(i));
+	}
+}
+
+}
+
+int main(int argc, char *argv[])
+{
+	bool list(false);
+	std::string testname;
+	char opt;
+	while ((opt = getopt(argc, argv, "lt:")) != -1) {
+		switch (opt) {
+		case 'l':
+			list = true;
+			break;
+		case 't':
+			testname = optarg;
+			break;
+		default: /* '?' */
+			std::cerr <<  "Usage:" << argv[0] << " [-t name] [-l]\n";
+			return EXIT_FAILURE;
+		}
+	}
+
 	CppUnit::TestFactoryRegistry & registry(
 		CppUnit::TestFactoryRegistry::getRegistry());
 	CppUnit::Test *test(registry.makeTest());
+
+	if (list) {
+		printTestTree(test);
+		return EXIT_SUCCESS;
+	}
+
+	CppUnit::TextTestRunner runner;
 	runner.addTest(test);
 
 	CppUnit::BriefTestProgressListener progress;
@@ -18,5 +54,5 @@ int main()
 	runner.setOutputter(new CppUnit::CompilerOutputter(
 		&runner.result(), std::cerr, "%p:%l:"));
 
-	return runner.run() ? 0 : 1;
+	return runner.run(testname) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
