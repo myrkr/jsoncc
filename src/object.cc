3999ac48f7ad98c6b23330e1c5779a842062cc35
Andreas Fett <a.fett@gmx.de>
2018-01-14 22:46:30 +0100

    update copyright

--- src/object.cc
+++ src/object.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2015 - 2017 Andreas Fett. All rights reserved.
+   Copyright (c) 2015 - 2018 Andreas Fett. All rights reserved.
    Use of this source code is governed by a BSD-style
    license that can be found in the LICENSE file.
  */
